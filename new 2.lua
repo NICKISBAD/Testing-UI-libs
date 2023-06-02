73bf2a9da51697e9d9de7fec82ab6c07aa615608
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-06-03 02:29:01 +0500

    Update new 2.lua

--- new 2.lua
+++ new 2.lua
@@ -1,17 +1,17 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/InfinitiveUI",true))()
 
 --Lib:CreateWindow(name,DefTab,WinSize,function)
-local Win = Lib:CreateWindow("UTBB GUI",2,nil,nil)
+local Win = Lib:CreateWindow("UTBB GUI",30,nil,nil)
 local Tab,name = Win:CreateTab("UTBB GUI For Jake",function() end)
 
-Tab:CreateButton("Test2" ,function()
-game.Workspace.Game.Mobs.Dummy2.Enemy.Health = 0
-end)
-
-Tab:CreateButton("Test1" ,function()
+Tab:CreateButton("Kill Enemy1" ,function()
 game.Workspace.Game.Mobs.Dummy.Enemy.Health = 0
 end)
 
-Tab:CreateButton("Test3" ,function()
+Tab:CreateButton("Kill Enemy2" ,function()
+game.Workspace.Game.Mobs.Dummy2.Enemy.Health = 0
+end)
+
+Tab:CreateButton("Kill Enemy3" ,function()
 game.Workspace.Game.Mobs.Dummy3.Enemy.Health = 0
 end)
